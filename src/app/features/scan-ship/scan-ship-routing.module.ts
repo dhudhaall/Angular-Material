ef7e8a49806dd896895a5eee05134a449f1fa278
zachswdream <umarahmaddh@gmail.com>
2021-08-28 02:09:10 +0500

    add item

--- src/app/features/scan-ship/scan-ship-routing.module.ts
+++ src/app/features/scan-ship/scan-ship-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemMainComponent } from './add-item-main/add-item-main.component';
 import { AddItemComponent } from './add-item-main/add-item/add-item.component';
 
 const routes: Routes = [
   {
     path: 'addItem',
-    component:AddItemComponent
+    component:AddItemMainComponent
   }
 ];
 
--- src/app/shared/layout/content-view/content-view.component.ts
+++ src/app/shared/layout/content-view/content-view.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-view.component.scss']
 })
 export class ContentViewComponent implements OnInit {
-
+  hideBar = false;
+div1 = true;
+  div2 = false;
+  div3 = false;
+  div4 = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showHideSection(num:number){
+    switch (num) {
+      case 1:{
+        this.div1 = true;
+        this.div2 = false;
+        this.div3 = false;
+        this.div4 = false;
+        break;
+      }
+      case 2:{
+        this.div2 = true;
+        this.div1 = false;
+        this.div3 = false;
+        this.div4 = false;
+        break;
+      }
+      case 3:{
+        this.div3 = true;
+        this.div1 = false;
+        this.div2 = false;
+        this.div4 = false;
+        break;
+      }
+      case 4:{
+        this.div4 = true;
+         this.div1 = false;
+        this.div2 = false;
+        this.div3 = false;
+        break;
+      }
+    }
+  }
+
+  hideShowRightBar() {
+
+    console.log(this.hideBar);
+    this.hideBar =  !this.hideBar;
+  }
 }
