31573ed62118819189934673d417a657131c1582
zachswdream <umarahmaddh@gmail.com>
2021-08-27 20:47:22 +0500

    scan

--- src/app/features/scan-ship/scan-ship.module.ts
+++ src/app/features/scan-ship/scan-ship.module.ts
@@ -4,12 +4,14 @@ import { SharedModule} from 'src/app/shared/shared.module';
 import { ScanShipRoutingModule } from './scan-ship-routing.module';
 import { AddItemComponent } from './add-item-main/add-item/add-item.component';
 import { AddItemMainComponent } from './add-item-main/add-item-main.component';
+import { AddItemFiltersComponent } from './add-item-main/add-item-filters/add-item-filters.component';
 
 
 @NgModule({
   declarations: [
     AddItemComponent,
-    AddItemMainComponent
+    AddItemMainComponent,
+    AddItemFiltersComponent
   ],
   imports: [
     CommonModule,SharedModule,
