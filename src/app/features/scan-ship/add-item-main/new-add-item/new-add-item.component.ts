4af33039d083b98bb5efb04a5ed714d2c50b4b7c
zachswdream <umarahmaddh@gmail.com>
2021-08-28 22:09:21 +0500

    neww add item

---
+++ src/app/features/scan-ship/add-item-main/new-add-item/new-add-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-add-item',
+  templateUrl: './new-add-item.component.html',
+  styleUrls: ['./new-add-item.component.scss']
+})
+export class NewAddItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
