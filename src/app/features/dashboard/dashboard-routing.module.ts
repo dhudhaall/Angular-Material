1a4d1a3c2fefddb3d50bb146402d239ab6b4a450
DESKTOP-E2EBDN1\NAEEMULLAH <naeemullah.khan758@gmail.com>
2021-08-29 00:37:14 +0500

    dashboard done

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
       path: '',
-      component: DashboardComponent
+      component: DashboardMainComponent
   }
 ];
 
