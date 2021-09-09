4cf64966356431af126cd6e8eb4ee94d1453a4fb
DESKTOP-E2EBDN1\NAEEMULLAH <naeemullah.khan758@gmail.com>
2021-09-09 23:27:13 +0500

    shipmeent detail page

--- src/app/features/scan-ship/scan-ship-routing.module.ts
+++ src/app/features/scan-ship/scan-ship-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddItemMainComponent } from './add-item-main/add-item-main.component';
 import { AddToShipmentComponent } from './add-to-shipment/add-to-shipment.component';
 import { NewAddItemMainComponent } from './new-add-item-main/new-add-item-main.component';
+import { ShipmentDetailsComponent } from './shipment-details/shipment-details.component';
 import { ShipmentPileMainComponent } from './shipment-pile-main/shipment-pile-main.component';
 import { ShipmentPileComponent } from './shipment-pile-main/shipment-pile/shipment-pile.component';
 
@@ -23,6 +24,11 @@ const routes: Routes = [
   {
     path:'addtoshipment',
     component:AddToShipmentComponent
+  }
+  ,
+  {
+    path:'shpmentdetail',
+    component:ShipmentDetailsComponent
   }
 ];
 
