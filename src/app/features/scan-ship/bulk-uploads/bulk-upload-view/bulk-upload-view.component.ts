7526a42c8534337d2366cbc774b3d2508a352bb8
zachswdream <umarahmaddh@gmail.com>
2021-09-14 19:39:58 +0500

    bulk uplaod starts

---
+++ src/app/features/scan-ship/bulk-uploads/bulk-upload-view/bulk-upload-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bulk-upload-view',
+  templateUrl: './bulk-upload-view.component.html',
+  styleUrls: ['./bulk-upload-view.component.scss']
+})
+export class BulkUploadViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
