509ec0f7249d2e7911458cd71f257e8243cb83e2
DESKTOP-E2EBDN1\NAEEMULLAH <naeemullah.khan758@gmail.com>
2021-10-22 22:34:48 +0500

    radio-btn

--- src/app/features/app-suite/select-package/select-package.component.ts
+++ src/app/features/app-suite/select-package/select-package.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-package.component.scss']
 })
 export class SelectPackageComponent implements OnInit {
-
+  isShow = false;
+  isShow2 = false;
+  isShow3 = false;
   constructor() { }
 
   ngOnInit(): void {
